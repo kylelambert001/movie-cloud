2e8722feed836f56e9bfcc9b3bdd63f20020549b
kylelambert001 <kylelambert001@gmail.com>
2020-08-25 17:58:50 +0930

    installed dotenv package to initalise env variables. created promises folder to handle api requests

---
+++ src/api/promises.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const getTrending = (type) => {
+  return axios.get(
+    `https://api.themoviedb.org/3/trending/${type}/week?api_key=${API_KEY}`
+  );
+};
--- src/components/containers/HomeContainer.js
+++ src/components/containers/HomeContainer.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import HomeView from "../views/HomeView/HomeView";
 
+import { getTrending } from "../../api/promises.js";
+
 class HomeContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: null,
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    // getTrending("movie")
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    // getTrending("tv")
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   }
 
   render() {
